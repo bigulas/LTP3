36975e34ffaf451b7bcc9861c35a0fa885b7f854
aluno <aluno@aluno-desktop>
2013-03-21 08:41:42 -0300

    PROJETO LTP3  criacao das classes pessoa, endereco, email e telefone.

---
+++ JavaLibrary1/src/br/edu/ifnmg/tads/ltp3/pessoa.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.ltp3;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author aluno
+ */
+public class pessoa {
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.id;
+        hash = 97 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final pessoa other = (pessoa) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+    
+    public void setNome(String pnome) throws Exception {
+        if(pnome.length()>3 && pnome.length()<250 ){
+        nome=pnome;
+    }
+    
+    throw new Exception("Não pode haver nomes com menos de 3 letras"
+            + "e mais de 250 letras");
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "id=" + id + ", nome=" + nome + '}';
+    }
+}
